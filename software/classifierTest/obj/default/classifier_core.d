6b04615c8fc037ca37456d27571564f8e0a0698f
Russill Glover <russillg@gmail.com>
2015-04-07 13:10:01 -0700

    Added C files

---
+++ software/classifierTest/obj/default/classifier_core.d
@@ -0,0 +1,10 @@
+obj/default/classifier_core.o: ../../genreCompare/classifier_core.c \
+  ../../genreCompare/classifier_core.h \
+  ../classifierTest_bsp//HAL/inc/io.h \
+  ../classifierTest_bsp//HAL/inc/alt_types.h
+
+../../genreCompare/classifier_core.h:
+
+../classifierTest_bsp//HAL/inc/io.h:
+
+../classifierTest_bsp//HAL/inc/alt_types.h:
